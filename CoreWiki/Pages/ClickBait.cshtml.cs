fb6a9ce6e0dadcc6280329ecf99a90166de88657
polavapr <pavan.vnr@gmail.com>
2018-09-01 18:47:27 -0400

    003 - CoreWiki Assignment

---
+++ CoreWiki/Pages/ClickBait.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoreWiki.Pages
+{
+    public class ClickBaitModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

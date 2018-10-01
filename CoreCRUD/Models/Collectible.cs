bd9a73688988a96d3f37273647722481a4cac4bc
polavapr <pavan.vnr@gmail.com>
2018-09-30 21:31:41 -0400

    010 - Added Model Validations

--- CoreCRUD/Models/Collectible.cs
+++ CoreCRUD/Models/Collectible.cs
@@ -1,22 +1,52 @@
-
 using System;
-using System.Text;
-using System.Linq;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
 
-namespace CoreCRUD.Models
-{
-    public class Collectible
-    {
+namespace CoreCRUD.Models {
+    public class Collectible {
         [Key]
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength (10)]
+        [RegularExpression (@"[a-z]+")]
         public string Name { get; set; }
+
+        [CustomValidation (typeof (Collectible), "DateValidator")]
+        [Display (Name = "Date of Purchase")]
         public DateTime DateOfPurchase { get; set; }
+
+        [CustomValidation (typeof (Collectible), "CategoryValidator")]
         public string Category { get; set; }
+
+        [Range (10, 2000)]
         public decimal Price { get; set; }
         public int ManufacturerID { get; set; }
         public Manufacturer Manufacturer { get; set; }
+        public static ValidationResult DateValidator (DateTime? pDate, ValidationContext context) {
+            if (pDate == null) {
+                return ValidationResult.Success;
+            }
+
+            if (pDate < DateTime.Today) {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult ("Purchase date must be in the past");
+        }
+        public static ValidationResult CategoryValidator (string category, ValidationContext context) {
+            if (category == null) {
+                return ValidationResult.Success;
+            }
+
+            if (category != "Pavan") {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult (" Category cannot be Pavan");
+        }
+
     }
 }
-            
